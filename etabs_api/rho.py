1d999f3f81993fbcf2fedbba2450893d24f7bd8d
ebrahim <ebe79442114@yahoo.com>
2021-07-03 17:24:30 +0430

    CFACTOR: adding rho file function

---
+++ etabs_api/rho.py
@@ -0,0 +1,26 @@
+import sys
+import comtypes.client
+from pathlib import Path
+
+civiltools_path = Path(__file__).parent.parent
+sys.path.insert(0, str(civiltools_path))
+from etabs_api import functions
+
+
+def get_ex_ey_earthquake_name(SapModel):
+    x_names, y_names = functions.get_load_patterns_in_XYdirection(SapModel)
+    x_names = sorted(x_names)
+    y_names = sorted(y_names)
+    drift_load_patterns = functions.get_drift_load_pattern_names(SapModel)
+    for name in x_names:
+        if name not in drift_load_patterns:
+            x_name = name
+            break
+    for name in y_names:
+        if name not in drift_load_patterns:
+            y_name = name
+            break
+    return x_name, y_name
+    
+
+
