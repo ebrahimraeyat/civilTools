b006826d1b2c08fbaef4b25bf15315645feffed2
ebrahimraeyat <ebe79442114@yahoo.com>
2021-11-08 16:03:28 +0330

    adding show points in distance dialog

--- applications/cfactor/py_widget/tools/distance_between_two_points.py
+++ applications/cfactor/py_widget/tools/distance_between_two_points.py
@@ -14,6 +14,17 @@ class Form(base, window):
         self.setupUi(self)
         self.etabs = etabs
         self.fill_points()
+        self.show_button.clicked.connect(self.show_points)
+    
+    def show_points(self):
+        if len(self.point_list) == 0:
+            return
+        p1 = self.point_list.item(0).text()
+        p2 = self.point_list.item(1).text()
+        self.etabs.SapModel.SelectObj.ClearSelection()
+        self.etabs.SapModel.PointObj.SetSelected(p1, True)
+        self.etabs.SapModel.PointObj.SetSelected(p2, True)
+        self.etabs.SapModel.View.RefreshView()
 
     def fill_points(self):
         points_names = self.etabs.select_obj.get_selected_obj_type(1)
@@ -21,6 +32,7 @@ class Form(base, window):
         if points_names is None and line_name is None:
             message = 'Please Select Two Points or a Frame in ETBAS Model.'
             QMessageBox.information(None, 'Selection', message)
+            return
         if len(points_names) < 2 and not line_name:
             return
         if len(points_names) > 1:
@@ -29,13 +41,18 @@ class Form(base, window):
             p1, p2, _ = self.etabs.SapModel.FrameObj.GetPoints(line_name[0])
         self.point_list.clear()
         self.point_list.addItems([p1, p2])
+        return True
 
     def accept(self):
-        self.fill_points()
+        ret = self.fill_points()
+        if ret is None:
+            return
         p1 = self.point_list.item(0).text()
         p2 = self.point_list.item(1).text()
         self.etabs.set_current_unit('N', 'm')
         distance = self.etabs.points.get_distance_between_two_points_in_XY(p1, p2)
         self.dist.setValue(distance)
+        self.etabs.SapModel.SelectObj.ClearSelection()
+        self.etabs.SapModel.View.RefreshView()
 
 
