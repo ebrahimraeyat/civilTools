17e02d9b8f25dc7d8fda12f98312934da53fa165
ebrahimraeyat <ebe79442114@yahoo.com>
2022-02-02 15:57:06 +0330

    adding units to toolbars

--- CivilToolsStatusBar.py
+++ CivilToolsStatusBar.py
@@ -22,6 +22,24 @@ def setStatusIcons(show=True):
 
     import FreeCADGui
     from PySide import QtCore,QtGui
+    import etabs_obj
+
+    force_units = [ 'kgf', 'N', 'kN', 'tonf']
+    length_units = ['m', 'cm', 'mm']
+
+    def set_unit(action):
+        utext = action.text().replace("&","")
+        # unit = [0,4,1,3,7,5][unitsList.index(utext)]
+        action.parent().parent().parent().setText(utext)
+        etabs = etabs_obj.EtabsModel(backup=False)
+        if etabs.success:
+            mw = FreeCADGui.getMainWindow()
+            statuswidget = mw.findChild(QtGui.QToolBar,"CivilToolsStatusWidget")
+            force = statuswidget.force_label.text()
+            length = statuswidget.length_label.text()
+            print(force, length)
+            print('changed')
+            etabs.set_current_unit(force, length)
 
 
     def addonMgr():
@@ -102,6 +120,44 @@ def setStatusIcons(show=True):
             else:
                 statuswidget = QtGui.QToolBar()
                 statuswidget.setObjectName("CivilToolsStatusWidget")
+                # force menue
+                force_label = QtGui.QPushButton("force")
+                force_label.setObjectName("force_label")
+                force_label.setFlat(True)
+                menu = QtGui.QMenu(force_label)
+                g_force_units = QtGui.QActionGroup(menu)
+                for u in force_units:
+                    a = QtGui.QAction(g_force_units)
+                    a.setText(u)
+                    menu.addAction(a)
+                force_label.setMenu(menu)
+                g_force_units.triggered.connect(set_unit)
+                etabs = etabs_obj.EtabsModel(backup=False)
+                force, length = 'kgf', 'm'
+                if etabs.success:
+                    force, length = etabs.get_current_unit()
+                force_label.setText(force)
+                force_label.setToolTip(translate("civiltools","The preferred force unit"))
+                statuswidget.addWidget(force_label)
+                statuswidget.force_label = force_label
+                st.addPermanentWidget(statuswidget)
+                # length menue
+                length_label = QtGui.QPushButton("force")
+                length_label.setObjectName("length_label")
+                length_label.setFlat(True)
+                menu = QtGui.QMenu(length_label)
+                g_length_units = QtGui.QActionGroup(menu)
+                for u in length_units:
+                    a = QtGui.QAction(g_length_units)
+                    a.setText(u)
+                    menu.addAction(a)
+                length_label.setMenu(menu)
+                g_length_units.triggered.connect(set_unit)
+                etabs = etabs_obj.EtabsModel(backup=False)
+                length_label.setText(length)
+                length_label.setToolTip(translate("civiltools","The preferred length unit"))
+                statuswidget.addWidget(length_label)
+                statuswidget.length_label = length_label
 
                 # update notifier button (starts hidden)
                 updatebutton = QtGui.QPushButton()
@@ -115,7 +171,6 @@ def setStatusIcons(show=True):
                 QtCore.QObject.connect(updatebutton,QtCore.SIGNAL("pressed()"),addonMgr)
                 updatebutton.hide()
                 statuswidget.addWidget(updatebutton)
-                st.addPermanentWidget(statuswidget)
                 QtCore.QTimer.singleShot(2500, checkUpdates) # delay a bit the check for civilTools WB update...
         else:
             if statuswidget:
