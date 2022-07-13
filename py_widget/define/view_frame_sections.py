7720d052c9c2e78233342f214de896f2a8e00821
ebrahimraeyat <ebe79442114@yahoo.com>
2022-07-13 11:13:33 +0430

    overwrite concrete in section in export to etabs

--- py_widget/define/view_frame_sections.py
+++ py_widget/define/view_frame_sections.py
@@ -25,6 +25,13 @@ class Form(QtWidgets.QWidget):
         self.form.columns_tableview.clicked.connect(self.view_column_section)
         self.form.beams_tableview.clicked.connect(self.view_beam_section)
         self.form.export_to_etabs.clicked.connect(self.export_to_etabs)
+        self.form.concrete_checkbox.clicked.connect(self.concrete_clicked)
+
+    def concrete_clicked(self):
+        if self.form.concrete_checkbox.isChecked():
+            self.form.concrete_mats.setEnabled(True)
+        else:
+            self.form.concrete_mats.setEnabled(False)
 
     def fill_form(self):
         concretes = self.etabs.material.get_material_of_type(2)
@@ -73,6 +80,7 @@ class Form(QtWidgets.QWidget):
 
     def export_to_etabs(self):
         i = self.form.tabWidget.currentIndex()
+        use_concrete_mat = self.form.concrete_checkbox.isChecked()
         concrete_mat = self.form.concrete_mats.currentText()
         selected_rows = set()
         sections = []
@@ -87,6 +95,8 @@ class Form(QtWidgets.QWidget):
             else:
                 sections = self.form.columns_tableview.model().sections
             for section in sections:
+                if not use_concrete_mat:
+                    concrete_mat = section.Concrete_Name
                 self.etabs.prop_frame.create_concrete_column(
                     name = section.Label,
                     concrete = concrete_mat,
@@ -115,6 +125,8 @@ class Form(QtWidgets.QWidget):
             else:
                 sections = self.form.beams_tableview.model().sections
             for section in sections:
+                if not use_concrete_mat:
+                    concrete_mat = section.Concrete_Name
                 self.etabs.prop_frame.create_concrete_beam(
                     name = section.Label,
                     concrete = concrete_mat,
