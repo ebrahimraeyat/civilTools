0d9267280e7b721eed934b66ff93faded9b6008a
ebrahimraeyat <ebe79442114@yahoo.com>
2022-09-05 14:01:41 +0430

    Create columns from blocks and polylines

--- py_widget/define/define_axes.py
+++ py_widget/define/define_axes.py
@@ -1,14 +1,16 @@
 from pathlib import Path
+import math
 
 from PySide2 import  QtWidgets
-from PySide2.QtWidgets import QMessageBox
+from PySide2.QtWidgets import QMessageBox, QFileDialog
 from PySide2.QtCore import Qt
 
-import ezdxf
 
 import FreeCAD
 import FreeCADGui as Gui
-import Draft, Arch
+import Arch
+
+from freecad_py import dxf_funcs
 
 import civiltools_rc
 
@@ -20,22 +22,62 @@ class Form(QtWidgets.QWidget):
     def __init__(self, etabs):
         super(Form, self).__init__()
         self.form = Gui.PySideUic.loadUi(str(civiltools_path / 'widgets' / 'define' / 'create_axes_from_dxf.ui'))
-        self.ezdxf_doc = None
-        self.x_axis = None
-        self.y_axis = None
-        self.axis = None
+        self.dxf_filename = None
         self.etabs = etabs
+        self.dxf_content = None
         self.fill_levels()
         self.create_connections()
+        # self.set_dxf_options()
+
+    def getStandardButtons(self):
+        return int(QtWidgets.QDialogButtonBox.Cancel)
+
+    # def set_dxf_options(self):
+    #     p = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/Mod/Draft")
+    #     p.SetFloat("dxfScaling", 1000)
+    #     p.SetBool("dxfUseLegacyImporter", True)
+    #     p.GetBool("dxfGetOriginalColors", True)
+
+    # def import_dxf(self):
+    #     filename = self.form.filename.text()
+    #     if not filename:
+    #         return
+    #     importDXF.readPreferences()
+    #     doc = FreeCAD.ActiveDocument
+    #     importDXF.getDXFlibs()
+    #     gui = FreeCAD.GuiUp
+    #     if importDXF.dxfReader:
+    #         # groupname = str(Path(filename).name)
+    #         # importgroup = doc.addObject("App::DocumentObjectGroup", groupname)
+    #         # importgroup.Label = groupname
+    #         importDXF.processdxf(doc, filename)
+    #         # for l in layers:
+    #         #     importgroup.addObject(l)
+    #     else:
+    #         importDXF.errorDXFLib(gui)
+    #     if gui:
+    #         Gui.SendMsgToActiveView("ViewFit")
 
     def create_connections(self):
+        self.form.columns_from.currentIndexChanged.connect(self.column_from_changed)
         self.form.browse.clicked.connect(self.browse)
-        self.form.layer_checkbox.clicked.connect(self.layer_clicked)
-        self.form.refresh.clicked.connect(self.update_gui)
-        self.form.refresh_levels.clicked.connect(self.fill_levels)
+        # self.form.refresh.clicked.connect(self.update_gui)
+        # self.form.refresh_levels.clicked.connect(self.fill_levels)
         self.form.create_axis.clicked.connect(self.create_axis)
         self.form.export_to_etabs.clicked.connect(self.export_to_etabs)
         self.form.create_columns.clicked.connect(self.create_columns)
+
+    def column_from_changed(self):
+        read_from = self.form.columns_from.currentText()
+        self.form.column_names.clear()
+        if read_from == 'Block':
+            blocks = self.get_blocks()
+            self.form.column_names.addItems(blocks)
+            self.form.column_names.setEnabled(True)
+        elif read_from == 'Hatch':
+            self.form.column_names.setEnabled(False)
+            # hatches = self.get_hatches()
+            # self.form.column_names.addItems(hatches)
 
     def fill_levels(self):
         if self.etabs is None:
@@ -49,71 +91,62 @@ class Form(QtWidgets.QWidget):
             item.setFlags(item.flags() | Qt.ItemIsUserCheckable)
             item.setCheckState(Qt.Checked)
 
-    def layer_clicked(self):
-        if self.form.layer_checkbox.isChecked():
-            self.form.layer_combobox.setEnabled(True)
-        else:
-            self.form.layer_combobox.setEnabled(False)
 
-    def get_layers(self):
-        if self.ezdxf_doc is None:
+    def import_dxf_in_freecad(self):
+        if self.dxf_filename is None:
             return
-        msp = self.ezdxf_doc.modelspace()
-        layers = set()
-        for e in msp.query("LINE LWPOLYLINE"):
-            layers.add(e.dxf.layer)
-        return layers
-
-    def update_gui(self):
-        if self.ezdxf_doc is None:
-            return
-        self.clear_all()
         if FreeCAD.ActiveDocument is None:
             FreeCAD.newDocument('Grids')
-        msp = self.ezdxf_doc.modelspace()
-        if self.form.layer_checkbox.isChecked():
-            layer = self.form.layer_combobox.currentText()
-            lines = msp.query(f'LINE LWPOLYLINE[layer=="{layer}"]')
-        else:
-            lines = msp.query("LINE LWPOLYLINE")
-        for line in lines:
-            if hasattr(line, 'dxf') and hasattr(line.dxf, 'start'):
-                Draft.make_line(tuple(line.dxf.start), tuple(line.dxf.end))
-            elif hasattr(line, 'get_points'):
-                points = line.get_points()
-                p1 = (points[0][0], points[0][1], 0)
-                p2 = (points[1][0], points[1][1], 0)
-                Draft.make_line(p1, p2)
-
+        
+        draw_line = self.form.lines.isChecked()
+        draw_polyline = draw_line
+        draw_block = self.form.blocks.isChecked()
+        draw_hatches = self.form.hatches.isChecked()
+        self.dxf_content = dxf_funcs.ImportDXF(self.dxf_filename)
+        self.dxf_content.create_layer()
+        if draw_block:
+            self.dxf_content.draw_block()
+        if draw_line:
+            self.dxf_content.draw_line()
+        if draw_polyline:
+            self.dxf_content.draw_polyline()
+        if draw_hatches:
+            pass
         FreeCAD.ActiveDocument.recompute()
         Gui.Selection.clearSelection()
         Gui.SendMsgToActiveView("ViewFit")
 
     def create_columns(self):
-        if FreeCAD.ActiveDocument is None:
-            FreeCAD.newDocument('Grids')
-        self.clear_columns()
-        block = self.form.column_block.currentText()
-        if block == '':
-            return
-        msp = self.ezdxf_doc.modelspace()
-        width = self.form.col_size.value()
-        for i in msp.query(f'INSERT[name=="{block}"]'):
-            center = tuple(i.dxf.insert)
-            rot = i.dxf.rotation
-            col = Arch.makeStructure(length=width, width=width, height = 8 * width)
-            col.Placement = FreeCAD.Placement(
-            FreeCAD.Vector(*center),
-            FreeCAD.Rotation(FreeCAD.Vector(0,0,1),rot))
+        if self.form.columns_from.currentText() == 'Block':
+            block_name = self.form.column_names.currentText()
+        elif self.form.columns_from.currentText() == 'Hatch':
+            block_name = 'column'
+        for obj in FreeCAD.ActiveDocument.Objects:
+            if  (hasattr(obj, 'rotation') and
+                hasattr(obj, 'width') and
+                hasattr(obj, 'height') and
+                hasattr(obj, 'name')
+            ):
+                if block_name and obj.name != block_name:
+                    continue
+                else:
+                    center = obj.Shape.BoundBox.Center
+                    col = Arch.makeStructure(
+                        length=obj.width,
+                        width=obj.height,
+                        height = 8 * obj.width)
+                    col.Placement = FreeCAD.Placement(
+                    center,
+                    FreeCAD.Rotation(FreeCAD.Vector(0,0,1),obj.rotation))
         FreeCAD.ActiveDocument.recompute()
 
     def create_axis(self):
         if FreeCAD.ActiveDocument is None:
             return
         x_coordinates, y_coordinates = self.get_xy_coordinates()
-        if max((len(x_coordinates), len(y_coordinates))) == 0:
-            QMessageBox.Warning(None, 'Error', 'You Must have at least  1 line in X and Y directions.')
-            return
+        # if max((len(x_coordinates), len(y_coordinates))) == 0:
+        #     QMessageBox.Warning(None, 'Error', 'You Must have at least  1 line in X and Y directions.')
+        #     return
         x_style_numbering = self.form.x_style_numbering.currentText()
         y_style_numbering = self.form.y_style_numbering.currentText()
         x_length = max(x_coordinates) - min(x_coordinates)
@@ -129,60 +162,51 @@ class Form(QtWidgets.QWidget):
             for i, j in zip(y_coordinates[-2::-1], y_coordinates[-1:0:-1]):
                 dist = j - i
                 y_distances.append(dist)
-        if self.x_axis is None:
-            self.x_axis = Arch.makeAxis(len(x_distances), 1000)
-            self.x_axis.Label = 'XAxis'
-            self.x_axis.ViewObject.LineColor = (0.00,1.00,0.00)
-        self.x_axis.ViewObject.FontSize = int(x_length / 10)
-        self.x_axis.ViewObject.BubbleSize = self.x_axis.ViewObject.FontSize * 1.5
-        self.x_axis.Placement.Base = base
-        self.x_axis.ViewObject.NumberingStyle = x_style_numbering
-        self.x_axis.Distances = x_distances
-        self.x_axis.Length = 1.1 * y_length
+        x_axis = Arch.makeAxis(len(x_distances), 1000)
+        x_axis.Label = 'XAxis'
+        x_axis.ViewObject.LineColor = (0.00,1.00,0.00)
+        x_axis.ViewObject.FontSize = int(x_length / 10)
+        x_axis.ViewObject.BubbleSize = x_axis.ViewObject.FontSize * 1.5
+        x_axis.Placement.Base = base
+        x_axis.ViewObject.NumberingStyle = x_style_numbering
+        x_axis.Distances = x_distances
+        x_axis.Length = 1.1 * y_length
 
-        if self.y_axis is None:
-            self.y_axis = Arch.makeAxis(len(y_distances), 1000)
-            self.y_axis.Label = 'YAxis'
-            self.y_axis.ViewObject.LineColor = (1.00,0.00,0.00)
-        self.y_axis.ViewObject.FontSize = int(x_length / 10)
-        self.y_axis.ViewObject.BubbleSize = self.y_axis.ViewObject.FontSize * 1.5
-        self.y_axis.ViewObject.NumberingStyle = y_style_numbering
-        self.y_axis.Distances = y_distances
-        self.y_axis.Placement.rotate(FreeCAD.Vector(0,0,0), FreeCAD.Vector(0, 0, 1), -90)
-        self.y_axis.Placement.Base = base + FreeCAD.Vector(0, y_length, 0)
-        self.y_axis.Length = 1.1 * x_length
-        if self.axis is None:
-            self.axis = Arch.makeAxisSystem([self.x_axis, self.y_axis])
+        y_axis = Arch.makeAxis(len(y_distances), 1000)
+        y_axis.Label = 'YAxis'
+        y_axis.ViewObject.LineColor = (1.00,0.00,0.00)
+        y_axis.ViewObject.FontSize = int(x_length / 10)
+        y_axis.ViewObject.BubbleSize = y_axis.ViewObject.FontSize * 1.5
+        y_axis.ViewObject.NumberingStyle = y_style_numbering
+        y_axis.Distances = y_distances
+        y_axis.Placement.rotate(FreeCAD.Vector(0,0,0), FreeCAD.Vector(0, 0, 1), -90)
+        y_axis.Placement.Base = base + FreeCAD.Vector(0, y_length, 0)
+        y_axis.Length = 1.1 * x_length
+        Arch.makeAxisSystem([x_axis, y_axis])
 
     def get_xy_coordinates(self):
-        if self.form.selections.isChecked():
-            lines = Gui.Selection.getSelection()
-        else:
-            lines = FreeCAD.ActiveDocument.Objects
         x_coordinates = set()
         y_coordinates = set()
-        for line in lines:
-            if not hasattr(line, 'Start'):
-                continue
-            e = line.Shape.Edges[0]
-            vecs = e.tangentAt(0)
-            if abs(vecs.x) < 0.01 and (0.99 < abs(vecs.y) <1.01):
-                x_coordinates.add(line.Start.x)
-            elif abs(vecs.y) < 0.01 and (0.99 < abs(vecs.x) <1.01):
-                y_coordinates.add(line.Start.y)
+        for obj in FreeCAD.ActiveDocument.Objects:
+            doc = FreeCAD.ActiveDocument
+            for obj in doc.Objects:
+                if hasattr(obj, 'IfcType') and obj.IfcType == 'Column':
+                    base = obj.Placement.Base
+                    x_coordinates.add(round(base.x))
+                    y_coordinates.add(round(base.y))
+        
+        if self.form.selections.isChecked():
+            lines = Gui.Selection.getSelection()
+            for line in lines:
+                e = line.Shape.Edges[0]
+                vecs = e.tangentAt(0)
+                if abs(vecs.x) < 0.01 and (0.99 < abs(vecs.y) <1.01):
+                    x_coordinates.add(round(e.BoundBox.XMin))
+                elif abs(vecs.y) < 0.01 and (0.99 < abs(vecs.x) <1.01):
+                    y_coordinates.add(round(e.BoundBox.YMin))
+
         return sorted(x_coordinates), sorted(y_coordinates)
 
-    def clear_all(self):
-        doc = FreeCAD.ActiveDocument
-        if doc is None:
-            return
-        for obj in doc.Objects:
-            if hasattr(obj, 'IfcType') and obj.IfcType == 'Column':
-                continue
-            doc.removeObject(obj.Name)
-        self.x_axis = None
-        self.y_axis = None
-        self.axis = None
     
     def clear_columns(self):
         doc = FreeCAD.ActiveDocument
@@ -193,16 +217,32 @@ class Form(QtWidgets.QWidget):
                 doc.removeObject(obj.Name)
 
     def get_blocks(self):
-        if self.ezdxf_doc is None:
-            return
         blocks = set()
-        for block in self.ezdxf_doc.blocks:
-            blocks.add(block.name)
+        if self.dxf_content is None:
+            if FreeCAD.ActiveDocument is None:
+                return
+            else:
+                return
+        inserts = self.dxf_content.drawing.entities.get_type("insert")   
+        for i in inserts:
+            blocks.add(i.block)
         return blocks
+    
+    def get_hatches(self):
+        hatches = set()
+        if self.ezdxf_doc is None:
+            if FreeCAD.ActiveDocument is None:
+                return
+            else:
+                return
+        msp = self.ezdxf_doc.modelspace()  
+        q = msp.query("HATCH")      
+        for hatch in q:
+            hatches.add(hatch.dxf.pattern_name)
+        return hatches
 
     def browse(self):
         ext = '.dxf'
-        from PySide2.QtWidgets import QFileDialog
         filters = f"{ext[1:]} (*{ext})"
         filename, _ = QFileDialog.getOpenFileName(None, 'select file',
                                                 None, filters)
@@ -210,36 +250,24 @@ class Form(QtWidgets.QWidget):
             return
         if not filename.lower().endswith(ext):
             filename += ext
-        self.form.filename.setText(filename)
-        try:
-            self.ezdxf_doc = ezdxf.readfile(filename)
-            layers = self.get_layers()
-            self.form.layer_combobox.clear()
-            self.form.layer_combobox.addItems(layers)
-            blocks = self.get_blocks()
-            self.form.column_block.clear()
-            self.form.column_block.addItems(blocks)
-            if FreeCAD.ActiveDocument is None:
-                FreeCAD.newDocument('Grids')
-        except IOError:
-            print(f"Not a DXF file or a generic I/O error.")
-        except ezdxf.DXFStructureError:
-            print(f"Invalid or corrupted DXF file.")
-        self.update_gui()
+        FreeCAD.newDocument()
+        self.dxf_filename = filename
+        self.import_dxf_in_freecad()
+        self.column_from_changed()
 
     def export_to_etabs(self):
         if self.etabs is None:
             QMessageBox.Warning(None, 'Open ETABS', 'Please open ETABS and run this command again.')
             return
 
-        len_unit = self.form.unit.currentText()
-        self.etabs.set_current_unit('N', len_unit)
+        # len_unit = self.form.unit.currentText()
+        self.etabs.set_current_unit('N', 'mm')
         self.etabs.unlock_model()
-        tab = self.form.tabWidget.currentIndex()
-        if tab == 0:
-            self.export_axes_to_etabs()
-        elif tab == 1:
-            self.export_columns_to_etabs()
+        g1 = self.export_axes_to_etabs()
+        n = self.export_columns_to_etabs()
+        msg = f'{n} columns added to current model and {g1} Grid Line Modifided.'
+        QMessageBox.information(None, 'Successful', msg)
+        self.etabs.SapModel.File.Save()
 
     def export_axes_to_etabs(self):
         grids = self.etabs.SapModel.GridSys.GetNameList()[1]
@@ -253,16 +281,19 @@ class Form(QtWidgets.QWidget):
             self.etabs.SapModel.GridSys.SetGridSys('G1', 0, 0, 0)
             g1 = 'G1'
         data = []
-        x_grids = get_axis_data(self.x_axis, 'X')
-        for coord, name in x_grids:
-            data.extend([g1, 'X (Cartesian)', name, str(coord), '', '', '', '', '', 'End', 'Yes'])
-        y_grids = get_axis_data(self.y_axis, 'Y')
-        for coord, name in y_grids:
-            data.extend([g1, 'Y (Cartesian)', name, str(coord), '', '', '', '', '', 'Start', 'Yes'])
+        x_axis, y_axis = get_xy_axis()
+        if x_axis:
+            x_grids = get_axis_data(x_axis, 'X')
+            for coord, name in x_grids:
+                data.extend([g1, 'X (Cartesian)', name, str(coord), '', '', '', '', '', 'End', 'Yes'])
+        if y_axis:
+            y_grids = get_axis_data(y_axis, 'Y')
+            for coord, name in y_grids:
+                data.extend([g1, 'Y (Cartesian)', name, str(coord), '', '', '', '', '', 'Start', 'Yes'])
         table_key = 'Grid Definitions - Grid Lines'
         fields = ['Name', 'Grid Line Type', 'ID', 'Ordinate', 'Angle', 'X1', 'Y1', 'X2', 'Y2', 'Bubble Location', 'Visible']
         self.etabs.database.apply_data(table_key, data, fields)
-        QMessageBox.information(None, 'Successful', f'{g1} Grid Line Modifided.')
+        return g1
 
     def export_columns_to_etabs(self):
         doc = FreeCAD.ActiveDocument
@@ -290,15 +321,28 @@ class Form(QtWidgets.QWidget):
                     name = self.etabs.SapModel.FrameObj.AddByCoord(x, y, level, x, y, next_level)[0]
                     self.etabs.SapModel.FrameObj.SetLocalAxes(name, rot)
                     n += 1
-        QMessageBox.information(None, 'Successful', f'{n} columns added to current model.')
-                    
-
-
-
-
+        return n
         
     def reject(self):
         Gui.Control.closeDialog()
+
+def get_xy_axis():
+    x_axis = None
+    y_axis = None
+    for obj in FreeCAD.ActiveDocument.Objects:
+        if (
+            hasattr(obj, 'Distances') and
+            hasattr(obj, 'Length') and
+            hasattr(obj, 'Angles')
+        ):
+            degree = math.degrees(obj.Placement.Rotation.Angle)
+            if degree == 0 or degree == 180:
+                if x_axis is None:
+                    x_axis = obj
+            elif degree == 90 or degree == 270:
+                if y_axis is None:
+                    y_axis = obj
+    return x_axis, y_axis
 
 def get_axis_data(obj, direction='X'):
     data = []
