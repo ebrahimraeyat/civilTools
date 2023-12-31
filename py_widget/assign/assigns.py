b443fd6fa83affe8b75f407f1ab7053244500843
ebrahimraeyat <ebe79442114@yahoo.com>
2023-12-31 22:26:47 +0330

    Set and check diaphragm for areas

--- py_widget/assign/assigns.py
+++ py_widget/assign/assigns.py
@@ -1,30 +1,49 @@
 from pathlib import Path
 
 from PySide2 import  QtWidgets
-# from PySide2.QtWidgets import QMessageBox
-from PySide2.QtGui import QPixmap
 from PySide2.QtGui import QIcon
 
 import FreeCADGui as Gui
 
 
 civiltools_path = Path(__file__).absolute().parent.parent.parent
+tick_icon = QIcon(str(civiltools_path / 'images' / 'tick.svg'))
 
 class Form(QtWidgets.QWidget):
     def __init__(self, etabs_model):
         super(Form, self).__init__()
         self.form = Gui.PySideUic.loadUi(str(civiltools_path / 'widgets' / 'assign' / 'assigns.ui'))
         self.etabs = etabs_model
+        self.fill_form()
         self.create_connections()
 
     def create_connections(self):
-        self.form.end_length_offset_pushbutton.clicked.connect(self.set_end_length_offset)
+        self.form.set_end_length_offset_pushbutton.clicked.connect(self.set_end_length_offset)
+        self.form.check_diaphragm_pushbutton.clicked.connect(self.check_diaphragm)
+        self.form.set_diaphragm_pushbutton.clicked.connect(self.set_diaphragm)
 
     def set_end_length_offset(self):
         value = self.form.end_length_offset_spinbox.value()
         self.etabs.frame_obj.set_end_length_offsets(value)
-        icon = QIcon(str(civiltools_path / 'images' / 'tick.svg'))
-        self.form.end_length_offset_pushbutton.setIcon(icon)
+        self.form.set_end_length_offset_pushbutton.setIcon(tick_icon)
+
+    def check_diaphragm(self):
+        if self.etabs.diaphragm.is_diaphragm_assigned():
+            self.form.check_diaphragm_pushbutton.setIcon(tick_icon)
+        else:
+            QtWidgets.QMessageBox.warning(None, 'Diaphragm', 'Diaphragm did not assigned.')
+
+    def set_diaphragm(self):
+        diaph_name = self.form.diaphragm_combobox.currentText()
+        if diaph_name not in self.etabs.diaphragm.names():
+            self.etabs.diaphragm.add_diaphragm(diaph_name)
+        self.etabs.diaphragm.set_area_diaphragms(diaph_name)
+        self.form.set_diaphragm_pushbutton.setIcon(tick_icon)
+
+    def fill_form(self):
+        diaphs = self.etabs.diaphragm.names()
+        if diaphs:
+            self.form.diaphragm_combobox.addItems(diaphs)
 
     def reject(self):
         self.form.close()    
