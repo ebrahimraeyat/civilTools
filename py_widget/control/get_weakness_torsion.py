80ef1781e9d897e9dd39458d8780d869cdb835e0
ebrahimraeyat <ebe79442114@yahoo.com>
2024-01-01 23:44:27 +0330

    Clean dialog in get torsion in weakend structure

--- py_widget/control/get_weakness_torsion.py
+++ py_widget/control/get_weakness_torsion.py
@@ -21,7 +21,7 @@ class Form(QtWidgets.QWidget):
         self.etabs = etabs_obj
         self.directory = str(Path(self.etabs.SapModel.GetModelFilename()).parent)
         self.fill_selected_beams()
-        self.set_filenames()
+        self.xy_radio_button_clicked()
         self.fill_xy_loadcase_names(d)
         self.create_connections()
 
@@ -48,32 +48,21 @@ class Form(QtWidgets.QWidget):
 
     def accept(self):
         asli_file_path = self.etabs.get_filename()
-        use_weakness_file = self.form.file_groupbox.isChecked()
         if self.form.x_radio_button.isChecked():
             dir_ = 'x'
             lw = self.form. x_loadcase_list
         else:
             dir_ = 'y'
             lw = self.form. y_loadcase_list
-        if use_weakness_file:
-            weakness_filepath = self.etabs.add_prefix_suffix_name(suffix=f'_weakness_torsion_{dir_}')
-            if weakness_filepath.exists() and \
-                self.etabs.get_filename() != weakness_filepath:
-                self.etabs.SapModel.File.OpenFile(str(weakness_filepath))
-            else:
-                weakness_filepath = Path(self.form.weakness_file.text())
-                if weakness_filepath.exists() and \
-                    self.etabs.get_filename() != weakness_filepath:
-                    self.etabs.SapModel.File.OpenFile(str(weakness_filepath))
+        items = self.form.beams_list.selectedItems()
+        if items:
+            name = items[-1].text()
         else:
-            items = self.form.beams_list.selectedItems()
-            if items:
-                name = items[-1].text()
-            else:
-                return
-            self.etabs.add_prefix_suffix_name(suffix=f'_weakness_torsion_{dir_}', open=True)
-            self.etabs.lock_and_unlock_model()
-            self.etabs.frame_obj.set_end_release_frame(name)
+            QtWidgets.QMessageBox.warning(None, 'Select Beam', 'Select one beam and press refresh button.')
+            return
+        self.etabs.add_prefix_suffix_name(suffix=f'_weakness_torsion_{dir_}', open=True)
+        self.etabs.lock_and_unlock_model()
+        self.etabs.frame_obj.set_end_release_frame(name)
         import table_model
         loadcases = []
         for i in range(lw.count()):
@@ -101,35 +90,21 @@ class Form(QtWidgets.QWidget):
             self.form.beams_list.addItems(beams)
             self.form.beams_list.setCurrentRow(len(beams) - 1)
 
-    def set_filenames(self):
+    def xy_radio_button_clicked(self):
         if self.form.x_radio_button.isChecked():
-            dir_ = 'x'
             self.form.x_loadcase_list.setEnabled(True)
             self.form.y_loadcase_list.setEnabled(False)
         elif self.form.y_radio_button.isChecked():
-            dir_ = 'y'
             self.form.x_loadcase_list.setEnabled(False)
             self.form.y_loadcase_list.setEnabled(True)
-        path = self.etabs.add_prefix_suffix_name(suffix=f'_weakness_torsion_{dir_}')
-        self.form.weakness_file.setText(str(path))
         
     def create_connections(self):
-        self.form.weakness_button.clicked.connect(self.get_filename)
         self.form.beams_list.itemClicked.connect(self.beam_changed)
         self.form.refresh_button.clicked.connect(self.fill_selected_beams)
-        self.form.x_radio_button.toggled.connect(self.set_filenames)
-        self.form.y_radio_button.toggled.connect(self.set_filenames)
+        self.form.x_radio_button.toggled.connect(self.xy_radio_button_clicked)
+        self.form.y_radio_button.toggled.connect(self.xy_radio_button_clicked)
         self.form.run.clicked.connect(self.accept)
 
     def beam_changed(self, item):
         beam_name = item.text()
         self.etabs.view.show_frame(beam_name)
-
-
-    def get_filename(self):
-        filename, _ = QFileDialog.getOpenFileName(self, 'select *.EDB file',
-                                                  self.directory, "ETBAS Model(*.EDB)")
-        if filename == '':
-            return
-        self.form.weakness_file.setText(filename)
-
